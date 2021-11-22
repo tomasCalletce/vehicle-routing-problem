707a3d19d1643af79a3faf099f1bb98dd6b2e37c
juan-c-s <juancamilo.salazar2002@gmail.com>
2021-11-22 12:33:33 -0500

    sizas

--- Map.java
+++ Map.java
@@ -7,42 +7,46 @@ public class Map {
     public double[][] graph;
     public Node[] nodes;
     public Node depot;
-    public HashMap<Integer,ArrayList<Node>> routes;
+    public HashMap<Integer, ArrayList<Node>> routes;
     public double speed;
     public double rate;
     public double capacidadMaximadebateriaendistancia;
 
-    
- 
-    public Map(int n,double speed,double rate){
+    public double shortestDistance = 100000;
+    public double longestDistance = 0;
+    public ArrayList<Node> shortestPath;
+
+    public Map(int n, double speed, double rate) {
         this.graph = new double[n][n];
         this.nodes = new Node[n];
         this.speed = speed;
         this.rate = rate;
         this.routes = new HashMap<>();
-
+        this.permutations = new ArrayList<ArrayList<Node>>();
     }
 
-    public void addDepot(int id,String nodoNombre,double x,double y,String tipoNodo,int tipoEstacion){
-        if(!nodoNombre.equals("depot")){
+    public void addDepot(int id, String nodoNombre, double x, double y, String tipoNodo, int tipoEstacion) {
+        if (!nodoNombre.equals("depot")) {
             System.out.println("ADD DEPOT");
-        }else{
-            this.depot = new Node(id, nodoNombre, x, y, tipoNodo, tipoEstacion,0);
+        } else {
+            this.depot = new Node(id, nodoNombre, x, y, tipoNodo, tipoEstacion, 0);
             this.nodes[id] = this.depot;
         }
 
     }
 
-    public void fillNode(int id,String nodoNombre,double x,double y,String tipoNodo,int tipoEstacion,double tiempoParaCargarAl100){
-        if(this.depot == null){
+    public void fillNode(int id, String nodoNombre, double x, double y, String tipoNodo, int tipoEstacion,
+            double tiempoParaCargarAl100) {
+        if (this.depot == null) {
             System.out.println("ADD DEPOT");
-        }else{
-            this.nodes[id] = new Node(id, nodoNombre, x - this.depot.x, y - this.depot.y, tipoNodo, tipoEstacion,tiempoParaCargarAl100);
+        } else {
+            this.nodes[id] = new Node(id, nodoNombre, x - this.depot.x, y - this.depot.y, tipoNodo, tipoEstacion,
+                    tiempoParaCargarAl100);
         }
-        
+
     }
 
-    public void fillGraph(){
+    public void fillGraph() {
 
         this.nodes[0].x = 0;
         this.nodes[0].y = 0;
@@ -52,7 +56,7 @@ public class Map {
 
             Node ni = null;
             for (int jj = 0; jj < graph.length; jj++) {
-                if(i == this.nodes[jj].id){
+                if (i == this.nodes[jj].id) {
                     ni = this.nodes[jj];
                 }
             }
@@ -61,103 +65,102 @@ public class Map {
                 Node nj = null;
 
                 for (int jj = 0; jj < graph.length; jj++) {
-                    if(j == this.nodes[jj].id){
+                    if (j == this.nodes[jj].id) {
                         nj = this.nodes[jj];
                     }
                 }
-                this.graph[i][j] = finder.pythagorean(ni.x,ni.y,nj.x,nj.y);
+                this.graph[i][j] = finder.pythagorean(ni.x, ni.y, nj.x, nj.y);
             }
         }
-    } 
+    }
 
-    private double getAngle(double x,double y){
+    private double getAngle(double x, double y) {
 
         double adder = 0;
-        if(x >= 0 && y >= 0){
+        if (x >= 0 && y >= 0) {
             adder = 0;
-        }else if(x < 0 && y > 0){
+        } else if (x < 0 && y > 0) {
             adder = 3.14159;
-        }else if(x < 0 && y < 0){
+        } else if (x < 0 && y < 0) {
             adder = 3.14159;
-        }else if(x > 0 && y < 0){
+        } else if (x > 0 && y < 0) {
             adder = 6.28319;
         }
 
-        return adder+Math.atan(y/x);
-
+        return adder + Math.atan(y / x);
 
     }
 
-    public void organizeNodesByAngle(){
+    public void organizeNodesByAngle() {
 
         int n = this.nodes.length;
         for (int i = 0; i < n; i++) {
-            this.nodes[i].angle = getAngle(this.nodes[i].x,this.nodes[i].y);
+            this.nodes[i].angle = getAngle(this.nodes[i].x, this.nodes[i].y);
         }
-        for (int i = 0; i < n-1; i++){
-            for (int j = 0; j < n-i-1; j++){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 Node n0 = this.nodes[j];
-                Node n1 = this.nodes[j+1];
-                if(getAngle(n0.x,n0.y) > getAngle(n1.x,n1.y)){
+                Node n1 = this.nodes[j + 1];
+                if (getAngle(n0.x, n0.y) > getAngle(n1.x, n1.y)) {
                     this.nodes[j] = n1;
-                    this.nodes[j+1] = n0;
+                    this.nodes[j + 1] = n0;
                 }
             }
         }
     }
 
-    public void printGraph(){
+    public void printGraph() {
 
         System.out.println("");
 
         for (int i = 0; i < graph.length; i++) {
             System.out.print("[");
             for (int j = 0; j < graph.length; j++) {
-                System.out.print(this.graph[i][j]+", ");
+                System.out.print(this.graph[i][j] + ", ");
             }
             System.out.println("]");
         }
 
         System.out.println("");
     }
-    
-    public int makeRoutes2(double range,double speed,double timpoMaximoDeRuta){
+
+    public int makeRoutes2(double range, double speed, double timpoMaximoDeRuta) {
 
         int routesCounter = 0;
         int nodeCounter = 1;
-        while(nodeCounter < this.nodes.length){
+        while (nodeCounter < this.nodes.length) {
             Stack<Node> sta = new Stack<>();
             sta.add(this.depot);
             double timeLeft = timpoMaximoDeRuta;
             double rangeLeft = range;
             double lastDistnaceToComeBack = 0;
-            while(timeLeft > 0 && rangeLeft > 0 && nodeCounter < this.nodes.length){
+            while (timeLeft > 0 && rangeLeft > 0 && nodeCounter < this.nodes.length) {
                 Node current = this.nodes[nodeCounter];
                 rangeLeft = rangeLeft - this.graph[sta.peek().id][current.id];
-                timeLeft = timeLeft - (this.graph[sta.peek().id][current.id]/speed);
-                if(current.tipoNodo.equals("s") && timeLeft > 0 && rangeLeft > 0){
+                timeLeft = timeLeft - (this.graph[sta.peek().id][current.id] / speed);
+                if (current.tipoNodo.equals("s") && timeLeft > 0 && rangeLeft > 0) {
                     timeLeft = timeLeft - current.tiempoDeCargaAl100;
                     rangeLeft = range;
                 }
                 sta.add(current);
                 rangeLeft = rangeLeft - this.graph[0][current.id];
-                timeLeft = timeLeft - (this.graph[0][current.id]/speed);
+                timeLeft = timeLeft - (this.graph[0][current.id] / speed);
                 rangeLeft = rangeLeft + lastDistnaceToComeBack;
                 lastDistnaceToComeBack = this.graph[0][current.id];
                 nodeCounter++;
             }
-            if(timeLeft < 0 || rangeLeft < 0){
+            if (timeLeft < 0 || rangeLeft < 0) {
 
-                if(sta.size() == 2){
+                if (sta.size() == 2) {
                     continue;
                 }
                 sta.pop();
                 sta.add(depot);
-                this.routes.put(routesCounter,new ArrayList<>(sta));
+                this.routes.put(routesCounter, new ArrayList<>(sta));
                 nodeCounter--;
-            }else{
+            } else {
                 sta.add(depot);
-                this.routes.put(routesCounter,new ArrayList<>(sta));
+                this.routes.put(routesCounter, new ArrayList<>(sta));
             }
 
             routesCounter++;
@@ -166,5 +169,59 @@ public class Map {
         return routesCounter;
     }
 
+    ArrayList<ArrayList<Node>> permutations;
 
+    public double TSP(ArrayList<Node> route, int index, ArrayList<Node> newRoute) {
+        if (index == route.size() - 1 || newRoute.size() == route.size() - 1) {
+            return this.graph[route.get(index - 1).id][route.get(index).id];
+        }
+        for (int i = 0; i < route.size(); i++) {
+            if (!visited[route.get(i).id]) {
+                visited[route.get(i).id] = true;
+
+            }
+        }
+
+        double d1 = TSP(route, index + 1, visited, newRoute);
+
+        double d2 = this.graph[route.get(index).id][route.get(index + 1).id] + TSP(route, index + 1, visited, newRoute);
+        if (d1 < d2) {
+            return d1;
+        }
+        return d2;
+    }
+
+    private static void bruteForceSearch(ArrayList<Node> cities, int startCity, double currentDistance) {
+
+        if (startCity < cities.size() - 1) {
+            for (int i = startCity; i < cities.size(); i++) {// cada iteración hace todas las permutaciones empezando
+                                                             // desde el iesimo, por eso es el swap
+                Node tempCity = cities.get(i);
+                cities.set(i, cities.get(startCity));
+                cities.set(startCity, tempCity);
+                currentDistance = computeDistance(cities);
+                bruteForceSearch(cities, startCity + 1, currentDistance);
+                tempCity = cities.get(i);
+                cities.set(i, cities.get(startCity));
+                cities.set(startCity, tempCity);
+            }
+        } else {
+            if (shortestDistance > currentDistance) {
+                shortestDistance = currentDistance;
+                shortestPath = new ArrayList<Node>(cities);
+            }
+            if (longestDistance < currentDistance) {
+                longestDistance = currentDistance;
+            }
+        }
+    }
+
+    private static double computeDistance(ArrayList<Node> cities) {
+        double distance = 0;
+        for (int i = 0; i < cities.size() - 1; i++) {
+            distance = distance + this.graph[cities.get(i).id][cities.get(i + 1).id];
+        }
+        distance = distance + this.graph[cities.get(cities.size() - 1).id][cities.get(0).id];
+        return distance;
+    }
 }
--- index.java
+++ index.java
@@ -1,36 +1,34 @@
 import java.util.ArrayList;
 
-public class index{
+public class index {
 
     public static void main(String[] args) {
 
-        Map map = ReadFiles.readFile("tc2c320s24cf0.txt",1.01);
-        double rangeOfMotor = ReadFiles.Q/ReadFiles.r;
+        Map map = ReadFiles.readFile("tc2c320s24cf0.txt", 1.01);
+        double rangeOfMotor = ReadFiles.Q / ReadFiles.r;
 
         System.out.println(rangeOfMotor);
-        //map.printGraph();
-
+        // map.printGraph();
 
         System.out.println();
 
         // for (int i = 0; i < ReadFiles.n; i++) {
-            
-        //     System.out.println("(" + map.nodes[i].x + "," + map.nodes[i].y + ")" + map.nodes[i].id);
+
+        // System.out.println("(" + map.nodes[i].x + "," + map.nodes[i].y + ")" +
+        // map.nodes[i].id);
         // }
 
         System.out.println();
-        
-        int routesNumber = map.makeRoutes2(rangeOfMotor,ReadFiles.speed,ReadFiles.Tmax);
+
+        int routesNumber = map.makeRoutes2(rangeOfMotor, ReadFiles.speed, ReadFiles.Tmax);
         for (int i = 0; i <= routesNumber; i++) {
             ArrayList<Node> lis = map.routes.get(i);
-            for (int j = 0;lis != null && j < lis.size(); j++) {
+            for (int j = 0; lis != null && j < lis.size(); j++) {
                 System.out.print(lis.get(j).id + " ");
             }
             System.out.println("  ");
         }
 
-        
     }
 
 }
-
